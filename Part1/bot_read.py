3b73b4299f1edee0427708242a9fb1a84b4628fb
Michael Swanson <mikeswanson12@msn.com>
2015-04-08 23:07:25 -0400

    update user agent name

--- Part1/bot_read.py
+++ Part1/bot_read.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import praw
 
-user_agent = ("PyEng Bot 0.1")
+user_agent = ("Ding-Bot 0.1")
 
 r = praw.Reddit(user_agent = user_agent)
 
-subreddit = r.get_subreddit("learnpython")
+subreddit = r.get_subreddit("all")
 
 for submission in subreddit.get_hot(limit = 5):
     print "Title: ", submission.title
